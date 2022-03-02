2890cb99453495055ccc4cdba31ee674e4bb235f
Reena Nagrale <reena.nagrale@kyndryl.com>
2022-03-02 23:29:38 +0530

    Updates

--- Gradient_descent.py
+++ Gradient_descent.py
@@ -3,6 +3,7 @@ Created on 28-Feb-2022
 
 @author: ReenaNagrale
 '''
+from _io import open
 '''
 Consider the last 4 digits of your mobile number (Note : In case there is a 0 in one of the digits replace it by 3). 
 Let it be n1n2n3n4. Generate a random matrix A of size n1n2 × n3n4. 
@@ -38,7 +39,8 @@ def CreateMatrix(row, col):
         
         col_list=[]
         for j in range(col):
-            col_list.append(random.randint(1,50))
+            #col_list.append(random.randint(1,50))
+            col_list.append(random.randrange(10000,99999)/10000)
         row_list.append(col_list)
     matrix_A = row_list
     return matrix_A
@@ -61,8 +63,8 @@ def Cal_InfinteNorm(matrix):
         row_sum[i]=temp_sum
     
     infnorm = max(row_sum)
-    print("\n Infinite Norm of Matrix is :",infnorm)
-    return
+    #print("\n Infinite Norm of Matrix is :",infnorm)
+    return infnorm
 
 '''
 Function to perform Matrix Transpose
@@ -139,10 +141,20 @@ def gradient_descent(previous_x, lrate, g, ATranspose, A,b):
     g_curr = mat_sub(mat_mul(mat_mul(ATranspose,A),previous_x), mat_mul(ATranspose,b)) 
     
     temp = [scalarProductMat([g_curr[i]],lrate)[0] for i in range(len(g_curr))]
-    print(temp)
+    #print(temp)
     curr_x = mat_sub(previous_x, temp)
-    print(curr_x)
+    #print(curr_x)
     return curr_x
+
+def fx_calculation(A_Mat,x,b_mat):
+    
+    temp=mat_sub(mat_mul(A_Mat,x),b_mat)
+    
+    temp = Cal_Frobeniusnorm(matrix=temp, rows=len(temp), cols=len(temp[0]))
+    
+    fx_value = (0.5 * pow(temp,2))
+    
+    return fx_value
 
 def main():
     num = input("Enter last four digit of your number: ")
@@ -150,38 +162,46 @@ def main():
     if int(num)< 9999 and int(num)> 1000:
         n1n2 = int(str(num)[:2])
         n3n4 = int(str(num)[2:4])
-        #matrix = CreateMatrix(row=n1n2, col=n3n4)
-        matrix=[[28, 32, 23, 25, 49, 10, 19, 32, 36, 19, 45, 7, 39, 47, 49, 24], [37, 41, 32, 26, 34, 7, 22, 12, 38, 13, 21, 25, 1, 20, 38, 3], [47, 49, 41, 11, 50, 47, 13, 30, 10, 18, 46, 6, 4, 3, 16, 13], [12, 32, 48, 35, 34, 1, 27, 2, 27, 49, 19, 41, 4, 34, 23, 22], [48, 14, 44, 43, 42, 18, 23, 3, 20, 6, 4, 1, 41, 19, 13, 5], [36, 41, 28, 33, 4, 27, 35, 44, 32, 35, 13, 3, 49, 30, 7, 20], [17, 15, 12, 27, 40, 9, 21, 28, 44, 4, 8, 4, 28, 3, 48, 44], [49, 28, 29, 9, 28, 28, 16, 33, 7, 33, 47, 6, 18, 17, 22, 40], [8, 18, 7, 47, 25, 50, 37, 49, 14, 2, 13, 9, 25, 27, 38, 47], [29, 19, 38, 46, 13, 2, 27, 22, 1, 3, 30, 26, 13, 11, 7, 43], [50, 15, 34, 42, 9, 18, 39, 11, 7, 1, 19, 13, 7, 22, 35, 45]]
-
-        print("\n Matrix Created with {} rows and {} columns".format(n1n2,n3n4))
-        print("\n", matrix)
-        InfiteNorm = Cal_InfinteNorm(matrix)
-        #b = CreateMatrix(n1n2,1)
-        b=[[48], [20], [7], [44], [18], [18], [20], [36], [14], [46], [1]]
-        print(b)
-        previous_x = [[1]*1]*n3n4
-        current_x = [[1]*1]*n3n4
-        i=0
-        GTranspose = mat_transpose(matrix)
-        #print(Cal_Frobeniusnorm(mat_sub(current_x,previous_x),n3n4,1))
-
-        while(Cal_Frobeniusnorm(mat_sub(current_x,previous_x),n3n4,1)<pow(10,-4)):
-            print(current_x)
-            g = mat_sub(mat_mul(mat_mul(GTranspose,matrix),current_x),mat_mul(GTranspose,b))
-            print("g",g)
-            lrate = learning_rate(g,GTranspose,matrix)
-            print("Learning Rate:",lrate)
-            previous_x = copy.deepcopy(current_x)
+        matrix = CreateMatrix(row=n1n2, col=n3n4)
+        print("Output is generated in file - Gradient_Descent_Output.txt")
+        with open("Gradient_Descent_Output.txt",'w') as outfile:
+        
+            print("\n Matrix Created with {} rows and {} columns".format(n1n2,n3n4),file=outfile)
+            print("\n", numpy.matrix(matrix),file=outfile)
+            InfiteNorm = Cal_InfinteNorm(matrix)
+            print("\n Infinite Norm of matrix = ",InfiteNorm,file=outfile)
+            b = CreateMatrix(n1n2,1)
+            print("\n Generated b matrix of size {} * 1".format(n1n2),file=outfile)
+            print("\n",numpy.matrix(b),file=outfile)
+            previous_x = [[0.0000000000001]*1]*n3n4
+            current_x = [[0.000000000000000001]*1]*n3n4
+            i=1
+            GTranspose = mat_transpose(matrix)
+            print("\n Frobenius norm of ||x(0) - x(-1)|| = ", Cal_Frobeniusnorm(mat_sub(current_x,previous_x),n3n4,1),file=outfile)
             
-            current_x = gradient_descent(current_x, lrate, g, GTranspose, matrix, b)
-            print("Current x:",i,current_x )
-            print("Previous x:", previous_x)
-            print(mat_sub(current_x,previous_x))
-            print(Cal_Frobeniusnorm(mat_sub(current_x,previous_x),n3n4,1))
-
+            while(Cal_Frobeniusnorm(mat_sub(current_x,previous_x),n3n4,1)<pow(10,-4)):
+                #print(current_x)
+            
+                g = mat_sub(mat_mul(mat_mul(GTranspose,matrix),current_x),mat_mul(GTranspose,b))
+                
+                lrate = learning_rate(g,GTranspose,matrix)
+                print("Learning Rate:",lrate, file=outfile)
+        
+                previous_x = copy.deepcopy(current_x)
+                
+                current_x = gradient_descent(current_x, lrate, g, GTranspose, matrix, b)
+                print("\n Iteration {} and x({}) : \n {} ".format(i,i,numpy.matrix(current_x)),file=outfile )
+                fx = fx_calculation(matrix,current_x,b)
+                print("\n f(x) value for generated x: ",fx, file=outfile)
+                print(mat_sub(current_x,previous_x))
+                print("\n Frobenius norm of ||x({}) - x({}-1)|| = {} ".format(i,i,Cal_Frobeniusnorm(mat_sub(current_x,previous_x),n3n4,1)),file=outfile)
+                print("\n Frobenius norm of ||x({}) - x({}-1)|| = {} ".format(i,i,Cal_Frobeniusnorm(mat_sub(current_x,previous_x),n3n4,1)))
+                
+                i+=1
+                
         
     else:
-        print("Number is not four digit!!!")
+        print("\n Number is not four digit!!!")
         
         
 if __name__=="__main__":
