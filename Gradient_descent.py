81e567ccd7f6971758675a20cc6f84d396336e0f
reenanagrale <34771480+reenanagrale@users.noreply.github.com>
2022-03-03 16:14:44 +0530

    Add files via upload

--- Gradient_descent.py
+++ Gradient_descent.py
@@ -11,12 +11,15 @@ For example, if the last four digits are 2311, generate a random matrix of size 
 Write a code to calculate the l∞ norm of this matrix. Deliverable(s) : The code that generates the results
 
 '''
+
 import random
 import numpy
 import copy
 from math import sqrt
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-def Cal_Frobeniusnorm(matrix,rows,cols):
+def Cal_L2norm(matrix,rows,cols):
     row = rows
     col = cols
     SumSqrt = 0
@@ -39,8 +42,8 @@ def CreateMatrix(row, col):
         
         col_list=[]
         for j in range(col):
-            #col_list.append(random.randint(1,50))
-            col_list.append(random.randrange(10000,99999)/10000)
+            col_list.append(random.randint(1,20))
+            #col_list.append(random.randrange(10000,99999)/10000)
         row_list.append(col_list)
     matrix_A = row_list
     return matrix_A
@@ -150,11 +153,27 @@ def fx_calculation(A_Mat,x,b_mat):
     
     temp=mat_sub(mat_mul(A_Mat,x),b_mat)
     
-    temp = Cal_Frobeniusnorm(matrix=temp, rows=len(temp), cols=len(temp[0]))
+    temp = Cal_L2norm(matrix=temp, rows=len(temp), cols=len(temp[0]))
     
-    fx_value = (0.5 * pow(temp,2))
+    fx_value = 0.5 * pow(temp,2)
     
     return fx_value
+
+def createplot(fx):
+    
+    steps = 0
+    fx_line = [i for i in fx if i>0]
+    steps = list(range(len(fx_line)))
+    plt.style.use('ggplot')
+    plt.figure(figsize = (20,8))
+    print(steps)
+    sns.lineplot(steps,fx_line)
+    plt.xlabel(xlabel='Steps')
+    plt.ylabel(ylabel='Cost')
+    plt.title("Gradient Descent Cost Function")
+    plt.show()
+    
+
 
 def main():
     num = input("Enter last four digit of your number: ")
@@ -163,8 +182,12 @@ def main():
         n1n2 = int(str(num)[:2])
         n3n4 = int(str(num)[2:4])
         matrix = CreateMatrix(row=n1n2, col=n3n4)
+        fx_list=[]
+        x_list=[]
+        #print("Learning Rate:",lrate, file=outfile)
+        
         print("Output is generated in file - Gradient_Descent_Output.txt")
-        with open("Gradient_Descent_Output.txt",'w') as outfile:
+        with open("Gradient_Descent_Output.txt",'a+') as outfile:
         
             print("\n Matrix Created with {} rows and {} columns".format(n1n2,n3n4),file=outfile)
             print("\n", numpy.matrix(matrix),file=outfile)
@@ -173,33 +196,39 @@ def main():
             b = CreateMatrix(n1n2,1)
             print("\n Generated b matrix of size {} * 1".format(n1n2),file=outfile)
             print("\n",numpy.matrix(b),file=outfile)
-            previous_x = [[0.0000000000001]*1]*n3n4
-            current_x = [[0.000000000000000001]*1]*n3n4
+            previous_x = CreateMatrix(row=n3n4, col=1)
+            current_x = CreateMatrix(row=n3n4, col=1)
             i=1
             GTranspose = mat_transpose(matrix)
-            print("\n Frobenius norm of ||x(0) - x(-1)|| = ", Cal_Frobeniusnorm(mat_sub(current_x,previous_x),n3n4,1),file=outfile)
-            
-            while(Cal_Frobeniusnorm(mat_sub(current_x,previous_x),n3n4,1)<pow(10,-4)):
+            print("\n L2 norm of ||x(0) - x(-1)|| = ", Cal_L2norm(mat_sub(current_x,previous_x),n3n4,1),file=outfile)
+            x_list.append(current_x)
+            fx = fx_calculation(matrix,current_x,b)
+            fx_list.append(fx)
+            g = mat_sub(mat_mul(mat_mul(GTranspose,matrix),current_x),mat_mul(GTranspose,b))
+                
+            lrate = learning_rate(g,GTranspose,matrix)
+        
+            while(Cal_L2norm(mat_sub(previous_x,current_x),n3n4,1)>pow(10,-4)):
                 #print(current_x)
             
-                g = mat_sub(mat_mul(mat_mul(GTranspose,matrix),current_x),mat_mul(GTranspose,b))
-                
-                lrate = learning_rate(g,GTranspose,matrix)
-                print("Learning Rate:",lrate, file=outfile)
-        
+                        
                 previous_x = copy.deepcopy(current_x)
                 
                 current_x = gradient_descent(current_x, lrate, g, GTranspose, matrix, b)
                 print("\n Iteration {} and x({}) : \n {} ".format(i,i,numpy.matrix(current_x)),file=outfile )
+                
                 fx = fx_calculation(matrix,current_x,b)
                 print("\n f(x) value for generated x: ",fx, file=outfile)
-                print(mat_sub(current_x,previous_x))
-                print("\n Frobenius norm of ||x({}) - x({}-1)|| = {} ".format(i,i,Cal_Frobeniusnorm(mat_sub(current_x,previous_x),n3n4,1)),file=outfile)
-                print("\n Frobenius norm of ||x({}) - x({}-1)|| = {} ".format(i,i,Cal_Frobeniusnorm(mat_sub(current_x,previous_x),n3n4,1)))
+                
+                x_list.append(current_x)
+                fx_list.append(round(fx,4))
+                
+                print("\n L2 norm of ||x({}) - x({}-1)|| = {} ".format(i,i,Cal_L2norm(mat_sub(current_x,previous_x),n3n4,1)),file=outfile)
+                #print("\n L2 norm of ||x({}) - x({}-1)|| = {} ".format(i,i,Cal_L2norm(mat_sub(current_x,previous_x),n3n4,1)))
                 
                 i+=1
-                
-        
+        print(fx_list)        
+        createplot(fx_list)
     else:
         print("\n Number is not four digit!!!")
         
